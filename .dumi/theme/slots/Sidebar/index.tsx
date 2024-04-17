5b982995893951d908d15945b0565f61ca76e8f2
hzj <hezhijian249@qq.com>
2024-04-17 23:51:05 +0800

    feat: 不需要分组了

--- .dumi/theme/slots/Sidebar/index.tsx
+++ .dumi/theme/slots/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useFullSidebarData, useLocation, useRouteMeta } from 'dumi';
+import { NavLink, useFullSidebarData, useLocation } from 'dumi';
 import React, { memo } from 'react';
 
 import { ISidebarGroup } from 'dumi/dist/client/theme-api/types';
@@ -13,17 +13,6 @@ const Sidebar: React.FC = () => {
   const paths = location.pathname.split('/').filter((item) => item);
   const activeKey: string = `/${paths[0]}`;
   let activeSidebar: ISidebarGroup[] = sidebar[activeKey] || [];
-  if (activeKey === '/components') {
-    const routeMeta = useRouteMeta();
-    const title =
-      typeof routeMeta.frontmatter.group === 'object'
-        ? routeMeta.frontmatter.group.title
-        : routeMeta.frontmatter.group;
-    const childGroup = activeSidebar.find((item) => item.title === title);
-    if (childGroup) {
-      activeSidebar = [childGroup];
-    }
-  }
   const isEmptySideBar = !fullSidebar;
   return isEmptySideBar ? null : (
     <div className={styles.sidebar}>
