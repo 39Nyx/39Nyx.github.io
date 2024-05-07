d673e8e539218b09bedc06154187aa829c605703
hzj <hezhijian249@qq.com>
2024-05-07 23:06:26 +0800

    docs: 支持其他语言的显示

--- .dumi/theme/builtins/SourceCode/index.tsx
+++ .dumi/theme/builtins/SourceCode/index.tsx
@@ -1,9 +1,29 @@
+import Editor from '@monaco-editor/react';
 import OriginSourceCode, {
   ISourceCodeProps,
 } from 'dumi/theme-default/builtins/SourceCode';
 import React from 'react';
 
 const SourceCode: React.FC<ISourceCodeProps> = (props) => {
-  return <OriginSourceCode {...props}>{props.children}</OriginSourceCode>;
+  const simpleLanguages: string[] = ['text'];
+  if (!props.lang || simpleLanguages.includes(props.lang)) {
+    return <OriginSourceCode {...props}>{props.children}</OriginSourceCode>;
+  }
+  const lineCount = props.children.split('\n').length;
+  const languageMap: any = {
+    ts: 'typescript',
+    tsx: 'typescript',
+  };
+  return (
+    <Editor
+      height={`${lineCount * 18 + 20}px`}
+      language={languageMap[props.lang] || props.lang}
+      defaultValue={props.children}
+      options={{
+        readOnly: true,
+        wordWrap: 'on',
+      }}
+    />
+  );
 };
 export default SourceCode;
