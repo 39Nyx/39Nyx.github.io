06ff3ec9398de69548a215bada96d4022648d454
hzj <hezhijian249@qq.com>
2024-11-15 23:11:12 +0800

    feat: 新增全局样式文件，并在配置文件中引入该样式。

--- .dumirc.ts
+++ .dumirc.ts
@@ -78,6 +78,9 @@ export default defineConfig({
   headScripts: [
     'window.EXCALIDRAW_ASSET_PATH = "/draw/";'
   ],
+  styles: [
+    `/styles/global.css`
+  ],
   favicons: [
     'https://gw.alipayobjects.com/zos/hitu-asset/c88e3678-6900-4289-8538-31367c2d30f2/hitu-1609235995955-image.png'
   ],
