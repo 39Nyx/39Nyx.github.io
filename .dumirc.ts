8a544bdb47a7b9af493370fc3444fe3092eb1c7e
hzj <hezhijian249@qq.com>
2024-12-22 23:43:39 +0800

    docs: 将nav单独提取出来到一个文件中

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'dumi';
+import { nav } from './config/nav';
 
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
@@ -10,68 +11,7 @@ export default defineConfig({
     logo: 'https://gw.alipayobjects.com/zos/hitu-asset/c88e3678-6900-4289-8538-31367c2d30f2/hitu-1609235995955-image.png',
     lastUpdated: true,
     showLineNum: true,
-    nav: [
-      {
-        title: '基础',
-        link: '/basic',
-        children: [
-          { title: 'JavaScript', link: '/js' },
-          { title: 'TypeScript', link: '/ts' },
-          { title: '算法', link: '/algorithm/question/dynamic-programming' }
-        ]
-      },
-      {
-        title: '框架',
-        link: '/framework',
-        children: [
-          { title: 'React', link: '/react' },
-          { title: 'Vue', link: '/vue' },
-          { title: 'Vue2', link: '/vue2' },
-          { title: 'Angular', link: '/angular' }
-        ]
-      },
-      {
-        title: '工具',
-        link: '/tools',
-        children: [
-          { title: 'Git', link: '/git' },
-          { title: 'Webstorm', link: '/webstorm' },
-          { title: 'npm', link: '/npm' },
-          { title: 'yarn', link: '/yarn' },
-          { title: 'pnpm', link: '/pnpm' },
-          { title: 'dUmi', link: '/d-umi' },
-          { title: 'github', link: '/github' }
-        ]
-      },
-      {
-        title: '库',
-        link: '/lib',
-        children: [
-          { title: 'ant design pro', link: '/ant' },
-          { title: 'umi.js', link: '/umi' },
-          { title: '拖拽', link: '/react-sortablejs/sortable' },
-          { title: 'unoCss', link: '/unoCss' },
-          { title: 'bpmnJs', link: '/bpmn-js' }
-        ]
-      },
-      {
-        title: '可视化',
-        link: '/visualization',
-        children: [
-          { title: 'webgl', link: '/webgl' },
-          { title: 'echarts', link: '/echarts' },
-          { title: 'three.js', link: '/three' }
-        ]
-      },
-      {
-        title: '样例',
-        link: '/components',
-        children: [
-          { title: 'ant', link: '/components/pro-layout' },
-          { title: 'echarts', link: '/components/line-chart' }
-        ]
-      }
-    ],
+    nav: nav,
     github: 'https://github.com/39Nyx/39Nyx.github.io'
   },
   html2sketch: {},
---
+++ config/nav.ts
@@ -0,0 +1,63 @@
+export const nav = [
+  {
+    title: '基础',
+    link: '/basic',
+    children: [
+      { title: 'JavaScript', link: '/js' },
+      { title: 'TypeScript', link: '/ts' },
+      { title: '算法', link: '/algorithm/question/dynamic-programming' }
+    ]
+  },
+  {
+    title: '框架',
+    link: '/framework',
+    children: [
+      { title: 'React', link: '/react' },
+      { title: 'Vue', link: '/vue' },
+      { title: 'Vue2', link: '/vue2' },
+      { title: 'Angular', link: '/angular' }
+    ]
+  },
+  {
+    title: '工具',
+    link: '/tools',
+    children: [
+      { title: 'Git', link: '/git' },
+      { title: 'Webstorm', link: '/webstorm' },
+      { title: 'npm', link: '/npm' },
+      { title: 'yarn', link: '/yarn' },
+      { title: 'pnpm', link: '/pnpm' },
+      { title: 'dUmi', link: '/d-umi' },
+      { title: 'github', link: '/github' }
+    ]
+  },
+  {
+    title: '库',
+    link: '/lib',
+    children: [
+      { title: 'ant design pro', link: '/ant' },
+      { title: 'umi.js', link: '/umi' },
+      { title: '拖拽', link: '/react-sortablejs/sortable' },
+      { title: 'unoCss', link: '/unoCss' },
+      { title: 'bpmnJs', link: '/bpmn-js' },
+      { title: 'axios', link: '/axios'}
+    ]
+  },
+  {
+    title: '可视化',
+    link: '/visualization',
+    children: [
+      { title: 'webgl', link: '/webgl' },
+      { title: 'echarts', link: '/echarts' },
+      { title: 'three.js', link: '/three' }
+    ]
+  },
+  {
+    title: '样例',
+    link: '/components',
+    children: [
+      { title: 'ant', link: '/components/pro-layout' },
+      { title: 'echarts', link: '/components/line-chart' }
+    ]
+  }
+]
