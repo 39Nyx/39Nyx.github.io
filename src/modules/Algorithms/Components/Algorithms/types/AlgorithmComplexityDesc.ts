30e63ebe5c290e46a107d484af2f989d20c1fd25
hzj <hezhijian249@qq.com>
2025-03-20 17:24:59 +0800

    feat: 定义AlgorithmComplexityDesc类

---
+++ src/modules/Algorithms/Components/Algorithms/types/AlgorithmComplexityDesc.ts
@@ -0,0 +1,18 @@
+type AlgorithmComplexityDesc = {
+  /**
+   * 情况
+   */
+  condition: string;
+  /**
+   * 时间复杂度
+   */
+  timeComplexity: string;
+  /**
+   * 空间复杂度
+   */
+  spaceComplexity: string;
+  /**
+   * 说明
+   */
+  explain: string;
+}
