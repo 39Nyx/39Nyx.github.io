cca2b119d7ec6ad73d2329ac8fcb8b74171231cd
hzj <hezhijian249@qq.com>
2025-03-26 23:19:57 +0800

    feat: 去掉prism，默认使用shiki

--- .dumi/theme/components/Highlighter/Highlighter.tsx
+++ .dumi/theme/components/Highlighter/Highlighter.tsx
@@ -15,42 +15,32 @@ type SyntaxHighlighterProps = Pick<
 >;
 
 const SyntaxHighlighter: React.FC<SyntaxHighlighterProps> = memo<SyntaxHighlighterProps>(
-  ({ children, language, type = 'shiki', syntaxThemes: syntaxTheme }) => {
+  ({ children, language, syntaxThemes: syntaxTheme }) => {
     const { styles, theme } = useStyles();
     const { isDarkMode } = useThemeMode();
     const [loading, setLoading] = useState(false);
 
     const codeToHtml = useShiki({ onLoadingChange: setLoading, theme: syntaxTheme?.shiki });
-    switch (type) {
-      case 'prism':
-        return (
-          <Flexbox className={styles.prism}>
-            <Prism language={language}>{children}</Prism>
-          </Flexbox>
-        );
-      default:
-      case 'shiki':
-        return (
-          <>
-            {loading ? (
-              <Prism language={language}>{children}</Prism>
-            ) : (
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: codeToHtml(children, language, isDarkMode) || '',
-                }}
-                className={styles.shiki}
-              />
-            )}
-            {loading && (
-              <Center horizontal gap={8} className={styles.loading}>
-                <Loading spin style={{ color: theme.colorTextTertiary }} />
-                着色器准备中...
-              </Center>
-            )}
-          </>
-        );
-    }
+    return (
+      <>
+        {loading ? (
+          <Prism language={language}>{children}</Prism>
+        ) : (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: codeToHtml(children, language, isDarkMode) || '',
+            }}
+            className={styles.shiki}
+          />
+        )}
+        {loading && (
+          <Center horizontal gap={8} className={styles.loading}>
+            <Loading spin style={{ color: theme.colorTextTertiary }} />
+            着色器准备中...
+          </Center>
+        )}
+      </>
+    );
   },
 );
 
