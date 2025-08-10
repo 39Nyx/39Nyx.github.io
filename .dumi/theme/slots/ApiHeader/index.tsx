60e041fc1f72a06a762adbb940fabfa63a8925f3
hzj <hezhijian249@qq.com>
2025-08-10 22:02:27 +0800

    feat: 移除自定义的内容

--- .dumi/theme/slots/ApiHeader/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { memo } from 'react';
-
-const ApiHeader: React.FC = () => {
-  return null;
-};
-
-export default memo(ApiHeader);
--- .dumi/theme/slots/SourceCodeEditor/style.ts
+++
@@ -1,7 +0,0 @@
-import { createStyles } from 'antd-style';
-
-export const useStyles = createStyles(({ css, token }) => ({
-  myContentClass: css`
-    background: rgba(173, 216, 230, 0.5);
-  `,
-}));
